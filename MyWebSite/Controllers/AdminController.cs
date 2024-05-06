3d40bff7e6b77451ae820f86dd87ff3ad1f15bcd
siphokuhlet <siphokuhlet@R135-03>
2024-05-06 07:53:38 +0200

    Add project files.

---
+++ MyWebSite/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AAdminDashboard()
+        {
+            return View();
+        }
+    }
+}
